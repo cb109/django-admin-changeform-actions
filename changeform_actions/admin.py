592dd29f09687949e1835aabe689fb53c05b4bb6
Christoph Buelter <christoph.buelter@myshopradio.de>
2025-11-27 13:56:10 +0100

    Render dropdown closer to how Django is doing it internally
    Will make it easier to follow Django updates in the future and be more
    resilient as the widget changes.

--- changeform_actions/admin.py
+++ changeform_actions/admin.py
@@ -1,8 +1,13 @@
-from typing import Callable, List, Optional, Tuple
+from typing import Optional
 
-from django.template import Template
 from django.template.context import ContextDict
+from django.template.loader import render_to_string
 from django.urls import reverse
+
+from .forms import ActionForm
+
+RUN_ACTION_URL: str = "run_admin_changeform_action"
+DEFAULT_DELETE_ACTION_NAME: str = "delete_selected"
 
 
 class ChangeFormActionsMixin:
@@ -39,18 +44,10 @@ class ChangeFormActionsMixin:
     """
 
     def get_form_action_url(self) -> str:
-        return reverse("run_admin_changeform_action")
+        return reverse(RUN_ACTION_URL)
 
-    def get_changeform_actions_dropdown(self, context) -> str:
+    def get_changeform_actions_dropdown(self, request, context) -> str:
         """Return HTML to run custom admin actions on the current object."""
-
-        actions: List[Tuple[Callable, str, str]] = [
-            (action, name, label)
-            for (action, name, label) in self._get_base_actions()
-            if name != "delete_selected"  # Skip the default delete action.
-        ]
-        if not actions:
-            return ""
 
         opts: dict = self.model._meta
         app_label: str = opts.app_label
@@ -58,63 +55,29 @@ class ChangeFormActionsMixin:
 
         object_id: Optional[int] = None
         for thing in context:
-            if isinstance(thing, ContextDict):
-                if "object_id" in thing:
-                    object_id = thing["object_id"]
-                    break
+            if not isinstance(thing, ContextDict):
+                continue
+            if "object_id" in thing:
+                object_id = thing["object_id"]
+                break
         if not object_id:
             return ""
 
-        def make_option_html_for_action(action_name: str, action_label: str) -> str:
-            return f"""
-              <option value="{action_name}">{action_label}</option>
-            """
+        action_form = ActionForm(auto_id=None)
+        action_form.fields["action"].choices = [
+            (name, label)
+            for (name, label) in self.get_action_choices(request)
+            if name != DEFAULT_DELETE_ACTION_NAME
+        ]
 
-        options_html: str = """
-          <option value="" selected>---------</option>
-        """
-        for _, action_name, action_label in actions:
-            options_html += make_option_html_for_action(action_name, action_label)
-
-        template_html: str = f"""
-          <li style="margin-left: 15px">
-            <form
-              action="{self.get_form_action_url()}"
-              method="post"
-            >
-              {{% csrf_token %}}
-              <input type="hidden" name="app_label" value="{app_label}">
-              <input type="hidden" name="model_name" value="{model_name}">
-              <input type="hidden" name="pk" value="{object_id}">
-              <label>
-                Aktion:
-                <select
-                  name="action"
-                  style="
-                    max-width: 240px;
-                    margin: 0;
-                    margin-left: 8px;
-                    margin-right: 2px;
-                  "
-                 >
-                  {options_html}
-                </select>
-                <button
-                  type="submit"
-                  onclick="this.disabled = true; this.form.submit();"
-                  class="button"
-                  style="
-                    height: 30px;
-                    padding: 4px 8px;
-                    margin: 0;
-                  "
-                >
-                  Los
-                </button>
-              </label>
-            </form>
-          </li>
-        """
-        return Template(template_html).render(context)
-        return Template(template_html).render(context)
-        return Template(template_html).render(context)
+        return render_to_string(
+            "admin/actions_for_changeform.html",
+            request=request,
+            context={
+                "action_form_url": self.get_form_action_url(),
+                "action_form": action_form,
+                "app_label": app_label,
+                "model_name": model_name,
+                "object_id": object_id,
+            },
+        )
--- changeform_actions/templatetags/changeform_actions.py
+++ changeform_actions/templatetags/changeform_actions.py
@@ -6,7 +6,8 @@ register = Library()
 @register.simple_tag(takes_context=True)
 def get_changeform_actions_dropdown(context, model_admin) -> str:
     """Return optional <select> HTML if ModelAdmin has been configured accordingly."""
+    request = context["request"]
     try:
-        return model_admin.get_changeform_actions_dropdown(context)
+        return model_admin.get_changeform_actions_dropdown(request, context)
     except AttributeError:
         return ""
