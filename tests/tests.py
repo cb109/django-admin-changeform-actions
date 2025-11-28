fd2e02ad7e551cec5c54e9ed62d5566b59d2f525
Christoph Buelter <christoph.buelter@myshopradio.de>
2025-11-28 09:59:54 +0100

    Add tests for POSTing to the view to run an action

--- tests/tests.py
+++ tests/tests.py
@@ -23,16 +23,67 @@ class TestIntegration:
         assert ChangeFormActionsMixin is not None
 
     def test_changeform_has_actions_dropdown(self):
-        # Create superuser and log in
+        # Setup
         User.objects.create_superuser("admin", "admin@example.com", "password")
         client = Client()
         client.login(username="admin", password="password")
 
-        # Create a model instance
         instance = MyModel.objects.create(name="Test")
 
-        # Get changeform page and check that the dropdown is present
-        changeform_url = reverse("admin:tests_mymodel_change", args=[instance.pk])
+        # Test: Get changeform page and check that the dropdown is present
+        changeform_url: str = reverse("admin:tests_mymodel_change", args=[instance.pk])
         response = client.get(changeform_url)
         assert response.status_code == 200
         assert b'<select name="action"' in response.content
+
+
+@pytest.mark.django_db
+class TestRunAction:
+    def test_run_action_needs_staff_user_login(self):
+        # Setup
+        User.objects.create_user("user", "user@example.com", "password")
+        client = Client()
+        client.login(username="user", password="password")
+
+        instance = MyModel.objects.create(name="Test")
+
+        # Test: Redirected to admin login instead
+        run_action_url: str = reverse("changeform_actions:run_admin_changeform_action")
+        response = client.post(
+            run_action_url,
+            data={
+                "app_label": "tests",
+                "model_name": "mymodel",
+                "pk": str(instance.pk),
+                "action": "add_copy_suffix",
+            },
+            HTTP_REFERER=reverse("admin:tests_mymodel_change", args=[instance.pk]),
+        )
+        assert response.status_code == 302
+        assert response.url.startswith(reverse("admin:login"))
+
+    def test_run_action(self):
+        # Setup
+        User.objects.create_superuser("admin", "admin@example.com", "password")
+        client = Client()
+        client.login(username="admin", password="password")
+
+        instance = MyModel.objects.create(name="Test")
+
+        # Test: Run action renames the model instance
+        run_action_url: str = reverse("changeform_actions:run_admin_changeform_action")
+        response = client.post(
+            run_action_url,
+            data={
+                "app_label": "tests",
+                "model_name": "mymodel",
+                "pk": str(instance.pk),
+                "action": "add_copy_suffix",
+            },
+            HTTP_REFERER=reverse("admin:tests_mymodel_change", args=[instance.pk]),
+            follow=True,
+        )
+        assert response.status_code == 200
+
+        instance.refresh_from_db()
+        assert instance.name == "Test - copy"
